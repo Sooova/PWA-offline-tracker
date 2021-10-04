d4a98eb2e0f1ad02829d281b16d4055785b53ec7
Sooova <83949034+Sooova@users.noreply.github.com>
2021-10-04 17:28:19 +1100

    update service worker
    cache items added

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+
+//which files needed to be cached
+const FILES_TO_CACHE = [
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.webmanifest',
+    '/index.js',
+    '/style.css',
+    '/db.js',
+];
