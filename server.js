0c3049b00303ab27d52dbceb09dcc2560289b284
Sooova <83949034+Sooova@users.noreply.github.com>
2021-11-05 21:33:25 +1100

    found that db wasn't linked

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-
-'use strict';
 const express = require("express");
-require('dotenv').config()
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -18,12 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // routes
 app.use(require("./routes/api.js"));
